978a8cf5089110c40db2f95ffaaa74098af2cb68
Alexander Thomas <6ftDwarf@gmail.com>
2018-11-13 17:57:09 -0800

    Add dysfunctional ECM scripts

---
+++ Assets/Scripts/ECS_Experiments/TestComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace ECS_Experiments
+{
+
+    [Serializable]
+    public struct Test : ISharedComponentData
+    {
+        public float moveSpeed;
+    }
+
+    public class TestComponent : SharedComponentDataWrapper<Test> { }
+
+}
---
+++ Assets/Scripts/ECS_Experiments/TestSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace ECS_Experiments
+{
+    public class TestSystem : ComponentSystem
+    {
+
+        ComponentGroup mainGroup;
+
+        protected override void OnCreateManager()
+        {
+            mainGroup = GetComponentGroup(typeof(TestComponent), typeof(Position));
+        }
+
+        protected override void OnUpdate()
+        {
+            ComponentDataArray<Position> compDatArray = mainGroup.GetComponentDataArray<Position>();
+            ComponentArray<TestComponent> compArray = mainGroup.GetComponentArray<TestComponent>();
+            for (int i = 0; i < compDatArray.Length; ++i)
+            {
+                float3 prevPos = compDatArray[i].Value;
+                compDatArray[i] = new Position
+                    { Value = prevPos + new float3(0f, Time.deltaTime * compArray[i].Value.moveSpeed, 0f) };
+            }
+        }
+
+    }
+}
