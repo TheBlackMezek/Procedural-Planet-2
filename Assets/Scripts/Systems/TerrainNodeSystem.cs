c51913790a9bfb4089bc7adc9655d2ff5983678b
Alexander Thomas <6ftDwarf@gmail.com>
2018-11-28 11:17:21 -0800

    Make test planet huge and clean up TerrainNodeSystem comments

--- Assets/Scripts/Systems/TerrainNodeSystem.cs
+++ Assets/Scripts/Systems/TerrainNodeSystem.cs
@@ -38,8 +38,6 @@ public class TerrainNodeSystem : ComponentSystem
         public void Execute()
         {
             Vector3[] vertices = new Vector3[nVerts];
-            //Vector3[] normals = new Vector3[nVerts];
-            //Vector2[] uvs = new Vector2[nVerts];
             int[] indices = new int[nTris * 3];
             
             float dist01 = Vector3.Distance(corner0, corner1);
@@ -64,8 +62,6 @@ public class TerrainNodeSystem : ComponentSystem
                     float noiseVal = GetValue(normal, node.noiseData, node.level);
                     vertices[vIdx] = normal * (node.planetData.radius + noiseVal * node.noiseData.finalValueMultiplier);
                     //vertices[vIdx] = normal * sphereRadius; //Use this line instead of above to gen a perfect sphere
-
-                    //normals[vIdx] = normal;
 
                     ++vIdx;
                 }
@@ -262,10 +258,6 @@ public class TerrainNodeSystem : ComponentSystem
 
                 Planet planetData = nodeArray[i].planetData;
 
-                //float3 corner1 = nodeArray[i].corner1 * planetData.radius;
-                //float3 corner2 = nodeArray[i].corner2 * planetData.radius;
-                //float3 corner3 = nodeArray[i].corner3 * planetData.radius;
-
                 // rez is the number of vertices on one side of the mesh/triangle
                 // the part in parentheses is called the "Mersenne Number"
                 int rez = 2 + ((int)Mathf.Pow(2, planetData.meshSubdivisions) - 1);
@@ -299,12 +291,7 @@ public class TerrainNodeSystem : ComponentSystem
                 mcs.verts = verts;
                 mcs.tris = tris;
                 meshCreationSets.Add(mcs);
-                
-                //Mesh mesh = BuildMesh(corners, planetData.meshSubdivisions, planetData.radius, nodeArray[i].noiseData, nodeArray[i].level);
-                //
-                //meshArray[i].mesh = mesh;
-                //Entity e = entityArray[i];
-                //EntityManager.SetSharedComponentData(e, meshArray[i]);
+
                 EntityManager.SetComponentData(entityArray[i], nodeArray[i]);
             }
         }
@@ -318,129 +305,6 @@ public class TerrainNodeSystem : ComponentSystem
             meshCreationSets[i].verts.Dispose();
             meshCreationSets[i].tris.Dispose();
         }
-    }
-
-
-
-    public static Mesh BuildMesh(Vector3[] corners, int divisions, float sphereRadius, PlanetNoise noiseData, int nodeLevel)
-    {
-        // rez is the number of vertices on one side of the mesh/triangle
-        // the part in parentheses is called the "Mersenne Number"
-        int rez = 2 + ((int)Mathf.Pow(2, divisions) - 1);
-        // nTris is the number of tris in the mesh
-        int t = rez - 2;
-        int nTris = (t * (t + 1)) + (rez - 1);
-        // nVerts is the number of vertices in the mesh
-        // it is the formula for the "Triangle Sequence" of numbers
-        int nVerts = (rez * (rez + 1)) / 2;
-        
-        Vector3[] vertices = new Vector3[nVerts];
-        Vector3[] normals = new Vector3[nVerts];
-        Vector2[] uvs = new Vector2[nVerts];
-        Color[] vColors = new Color[nVerts];
-        int[] indices = new int[nTris * 3];
-
-        float dist01 = Vector3.Distance(corners[0], corners[1]);
-        float dist12 = Vector3.Distance(corners[1], corners[2]);
-        float dist20 = Vector3.Distance(corners[2], corners[0]);
-
-        float lenAxis01 = dist01 / (rez - 1);
-        float lenAxis12 = dist12 / (rez - 1);
-        float lenAxis20 = dist20 / (rez - 1);
-
-        Vector3 add1 = (corners[1] - corners[0]).normalized * lenAxis01;
-        Vector3 add2 = (corners[2] - corners[1]).normalized * lenAxis12;
-
-        int vIdx = 0;
-
-        for (int i = 0; i < rez; ++i)
-        {
-            for (int n = 0; n <= i; ++n)
-            {
-                vertices[vIdx] = corners[0] + add1 * i + add2 * n;
-                Vector3 normal = (vertices[vIdx]).normalized;
-                float noiseVal = GetValue(normal, noiseData, nodeLevel);
-                vertices[vIdx] = normal * (sphereRadius + noiseVal * noiseData.finalValueMultiplier);
-                //vertices[vIdx] = normal * sphereRadius; //Use this line instead of above to gen a perfect sphere
-
-                normals[vIdx] = normal;
-
-                vColors[vIdx] = GetVertexColor(noiseData, noiseVal);
-
-                ++vIdx;
-            }
-        }
-
-        int indIdx = 0;
-        int rowStartIdx = 1;
-        int prevRowStartIdx = 0;
-
-        for (int row = 0; row < rez - 1; ++row)
-        {
-            bool upright = true;
-            int trisInRow = 1 + row * 2;
-            int vertsInRowBottom = row + 2;
-
-            int upTri = 0;
-            int downTri = 0;
-
-            for (int tri = 0; tri < trisInRow; ++tri)
-            {
-                if (upright)
-                {
-                    indices[indIdx] = rowStartIdx + upTri + 1;
-                    indices[indIdx + 1] = rowStartIdx + upTri;
-                    indices[indIdx + 2] = prevRowStartIdx + upTri;
-                    ++upTri;
-                }
-                else
-                {
-                    indices[indIdx] = prevRowStartIdx + downTri + 1;
-                    indices[indIdx + 1] = rowStartIdx + downTri + 1;
-                    indices[indIdx + 2] = prevRowStartIdx + downTri;
-                    ++downTri;
-                }
-
-                indIdx += 3;
-                upright = !upright;
-            }
-
-            prevRowStartIdx = rowStartIdx;
-            rowStartIdx += vertsInRowBottom;
-        }
-
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.triangles = indices;
-        mesh.colors = vColors;
-        //mesh.uv = uvs;
-        //mesh.normals = normals;
-        mesh.RecalculateNormals();
-        return mesh;
-    }
-
-
-
-    private static Color GetVertexColor(PlanetNoise noiseData, float heightFromSeaLevel)
-    {
-        int layerIdx = 0;
-
-        return Color.gray;
-
-        //int len = noiseData.colorLayers.Length;
-        //for (int i = 1; i < len; ++i)
-        //{
-        //    PlanetColorLayer layer = noiseData.colorLayers[i];
-        //
-        //    if (layer.heightThreshold < heightFromSeaLevel)
-        //        layerIdx = i;
-        //    else
-        //        break;
-        //}
-        //
-        //return new Color(noiseData.colorLayers[layerIdx].r,
-        //                 noiseData.colorLayers[layerIdx].g,
-        //                 noiseData.colorLayers[layerIdx].b);
     }
 
 
@@ -477,12 +341,6 @@ public class TerrainNodeSystem : ComponentSystem
 
             nodes[i].parentCenter = centerPoint;
             nodes[i].parnetSubdivideDist = distToSubdivide;
-
-            //GameObject child = Instantiate(parentSphere.NodePrefab);
-            //child.transform.position = transform.position;
-            //child.transform.rotation = transform.rotation;
-            //child.transform.parent = transform;
-            //children[i] = child.GetComponent<PlatosphereNode>();
         }
         
         float3 mid01 = corner1 - corner0;
@@ -516,12 +374,9 @@ public class TerrainNodeSystem : ComponentSystem
         for (int i = 0; i < 4; ++i)
             EntityManager.SetComponentData(entities[i], nodes[i]);
 
-        //r.mesh = null;
-
         t.divided = 1;
         t.built = 0;
-
-        //EntityManager.SetSharedComponentData(e, r);
+        
         EntityManager.SetComponentData(e, t);
     }
 
@@ -530,7 +385,6 @@ public class TerrainNodeSystem : ComponentSystem
     public static float GetValue(float x, float y, float z, PlanetNoise noiseData, int level = 0)
     {
         float ret = GetNoiseValue(x, y, z, noiseData, level);
-        //ret = settings.heightCurve.Evaluate(ret);
         
         return ret;
     }
@@ -547,7 +401,6 @@ public class TerrainNodeSystem : ComponentSystem
 
         for (int i = 0; i < noiseData.octaves + level * 1; ++i)
         {
-            //ret += noiseClass.GetValue(x * localFreq, y * localFreq, z * localFreq) * localAmp;
             ret += fastNoise.GetSimplex(x * localFreq, y * localFreq, z * localFreq) * localAmp;
 
             maxValue += localAmp;
