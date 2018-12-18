21babf0d45503765d191eef78086f7a482225d34
Alexander Thomas <6ftDwarf@gmail.com>
2018-12-17 17:58:24 -0800

    Begin adding hyperdist-hyperdist division

--- Assets/Scripts/Utilities/MathUtils.cs
+++ Assets/Scripts/Utilities/MathUtils.cs
@@ -121,28 +121,6 @@ public struct HyperDistance
         //return new HyperDistance { prs = a.prs, oct = oct + (int)octF };
     }
 
-    public static HyperDistance operator/(HyperDistance lhs, HyperDistance rhs)
-    {
-        float octantSize = HyperposStaticReferences.OctantSize;
-
-        //int oct = lhs.oct / rhs.oct;
-        int oct = 0;
-        float r = 0f;
-        if(rhs.oct != 0)
-        {
-            oct = lhs.oct / rhs.oct;
-            r = (float)lhs.oct % rhs.oct;
-        }
-        else
-        {
-
-        }
-        
-        float prs = (r * octantSize + lhs.prs) / rhs.prs;
-
-        return new HyperDistance { prs = prs, oct = oct };
-    }
-
     public static bool operator>(HyperDistance lhs, HyperDistance rhs)
     {
         bool ret = false;
